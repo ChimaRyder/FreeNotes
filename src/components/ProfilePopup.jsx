024dbbe45dbb808e52037163d9cdbb5c01d459d2
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2024-07-26 23:07:35 +0800

    small change to ProfilePopup so that the name loading doesnt feel lonely.

--- src/components/ProfilePopup.jsx
+++ src/components/ProfilePopup.jsx
@@ -21,16 +21,35 @@ const ProfilePopup = ({loading, username, onLogout}) => {
     return (
         <Card className={"border-0 shadow-none flex flex-col justify-center items-center bg-background"}>
             <CardHeader className={'!p-3 flex items-center'}>
-                <IconUserFilled size={80}/>
-                {loading && <CardTitle className={"!mt-5"}><Skeleton className={'w-[100px] h-[24px] rounded-md'}/></CardTitle>}
-                {!loading && <CardTitle className={"!mt-5"}>{username}</CardTitle>}
+                {loading &&
+                    <>
+                    <Skeleton className={'w-[80px] h-[80px] rounded-full'}/>
+                    <CardTitle className={"!mt-5"}><Skeleton className={'w-[100px] h-[24px] rounded-md'}/></CardTitle>
+                    </>
+                }
+                {!loading &&
+                    <>
+                    <IconUserFilled size={80}/>
+                    <CardTitle className={"!mt-5"}>{username}</CardTitle>
+                    </>
+                }
             </CardHeader>
             <CardContent className={'flex flex-col w-full space-y-2'}>
                 <Separator className={'my-3'}/>
-                <Button variant={'outline'} className={'w-max-full'} asChild>
-                    <Link to={'/dashboard/settings'}>Dashboard</Link>
-                </Button>
-                <Button variant={'outline'} className={'!w-max-full'} onClick={()=> {handleLogout()}}>Logout</Button>
+                {loading &&
+                    <>
+                    <Skeleton className={'h-10 w-max-full'}></Skeleton>
+                    <Skeleton className={'h-10 w-max-full'}></Skeleton>
+                    </>
+                }
+                {!loading &&
+                    <>
+                    <Button variant={'outline'} className={'w-max-full'} asChild>
+                        <Link to={'/dashboard/settings'}>Dashboard</Link>
+                    </Button>
+                    <Button variant={'outline'} className={'!w-max-full'} onClick={()=> {handleLogout()}}>Logout</Button>
+                    </>
+                }
             </CardContent>
         </Card>
     );
