1cb0ae45c800d6fc62aa598eb1b74ae215a4de6f
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2024-07-31 23:53:28 +0800

    barebones form for settings, will add switch for darkmode

--- src/components/dashboard-components/Settings.jsx
+++ src/components/dashboard-components/Settings.jsx
@@ -1,7 +1,64 @@
 import {IconSettings} from "@tabler/icons-react";
 import {Separator} from "@/components/ui/separator.jsx";
+import {z} from "zod"
+import {useEffect, useState} from "react";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import userAuthenticate from "@/components/middleware/userAuthenticate.jsx";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.jsx";
+import {Input} from "@/components/ui/input.jsx";
+
+const usernameSchema = z.object({
+    username: z
+        .string()
+        .min(1, "Please enter a valid username.")
+        .min(5, "Username should be at least 5 characters."),
+}).superRefine(async ({username}, ctx) => {
+    const userExists = async () => {
+        const res = await fetch(import.meta.env.VITE_API_LINK + "/userSearch?query=" + username)
+        const data = await res.json();
+        return data.userExists;
+    }
+
+    const x = await userExists().then((value) => {
+        return value;
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    if (x) {
+        ctx.addIssue({
+            code: "custom",
+            message: "Username already exists.",
+            path: ['username']
+        });
+    }
+});
 
 const Settings = () => {
+    const [loading, setLoading] = useState(false);
+    const [user, setUser] = useState([]);
+
+    const getUser = async () => {
+        const auth_user = await userAuthenticate();
+        setUser(auth_user);
+    }
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    const form = useForm({
+        resolver: zodResolver(usernameSchema),
+        defaultValues: {
+            username: "",
+        }
+    })
+
+    const handleSubmit = (user) => {
+
+    }
+
     return (
         <>
             <div className={'w-full flex flex-col p-10'}>
@@ -13,7 +70,23 @@ const Settings = () => {
                 <Separator className={'mt-5 mb-8'}/>
 
                 <div>
-                    <h3 className={'italic'}>Something is brewing...</h3>
+                    <Form {...form}>
+                        <form onSubmit={handleSubmit} className={"space-y-2 flex flex-col items-start"}>
+                            <FormField
+                                control = {form.control}
+                                name = "username"
+                                render = {({field}) =>(
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder={user.username} {...field}/>
+                                        </FormControl>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
                 </div>
             </div>
         </>
